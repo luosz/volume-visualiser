4f9444b002aea56efe716d9fe0e38bbae92f8003
Joe <Joe@localhost>
2014-01-29 18:02:34 +0000

    add python scripts

---
+++ voreen_time_varying/vortex_uchar.py
@@ -0,0 +1,26 @@
+# Voreen Python script
+import voreen
+import voreenqt
+
+voreen.info()
+voreenqt.info()
+
+voreen.setPropertyValue("VolumeSelector", "volumeID", 0)
+
+for i in range(0, 98):
+
+    voreen.setPropertyValue("VolumeSelector", "volumeID", i)
+
+    tf_filename = "../../../../output/vortex_tf/%02d.tfi" % i
+    
+    voreen.loadTransferFunction(
+    "SingleVolumeRaycaster", 
+    "transferFunction", 
+    tf_filename)
+    
+    print "loaded transfer function %s" % tf_filename
+    
+    image_filename = "../../../../output/vortex_image/%02d.png" % i
+    voreen.snapshotCanvas(0, image_filename)
+
+    print "saved to image %s" % image_filename
