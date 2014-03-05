30156066e017e366b936f0fc89faa993ccd77dd3
Joe <Joe@localhost>
2014-03-05 18:05:51 +0000

    add voreen files

---
+++ voreen_time_varying/isabel_QVAPOR_uchar.py
@@ -0,0 +1,25 @@
+# Voreen Python script
+import voreen
+import voreenqt
+
+voreen.info()
+voreenqt.info()
+
+voreen.setPropertyValue("VolumeSelector", "volumeID", 0)
+
+for i in range(0, 48):
+
+    voreen.setPropertyValue("VolumeSelector", "volumeID", i)
+
+    tf_filename = "../../../../output/isabel_QVAPOR/%02d.tfi" % i
+    
+    voreen.loadTransferFunction("SingleVolumeRaycaster", "transferFunction", tf_filename)
+    
+    print "loaded transfer function %s" % tf_filename
+
+    voreen.repaint()
+    
+    image_filename = "../../../../output/isabel_QVAPOR_output/%02d.png" % i
+    voreen.snapshotCanvas(0, image_filename)
+
+    print "saved to image %s" % image_filename
