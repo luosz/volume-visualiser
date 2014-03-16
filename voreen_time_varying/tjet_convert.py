9dd3feb6b2b1eed526322bf7991ab0f1d291a239
Joe <Joe@localhost>
2014-03-16 23:23:08 +0000

    update voreen workspace

--- voreen_time_varying/tjet_convert.py
+++ voreen_time_varying/tjet_convert.py
@@ -6,15 +6,9 @@ voreen.info()
 voreenqt.info()
 
 voreen.setPropertyValue("VolumeSelector", "volumeID", 0)
-
 for i in range(0, 150):
-
     voreen.setPropertyValue("VolumeSelector", "volumeID", i)
-
-    filename = "../../../../output/tjet/%03d.mhd" % i
-
+    filename = "../../../../_uchar/tjet/%03d.mhd" % i
     voreen.setPropertyValue("VolumeSave", "outputFilename", filename)
-
     voreen.repaint()
-
     print filename
--- voreen_time_varying/vortex_convert.py
+++ voreen_time_varying/vortex_convert.py
@@ -6,15 +6,9 @@ voreen.info()
 voreenqt.info()
 
 voreen.setPropertyValue("VolumeSelector", "volumeID", 0)
-
 for i in range(0, 98):
-
     voreen.setPropertyValue("VolumeSelector", "volumeID", i)
-
-    filename = "../../../../output/vortex/%02d.mhd" % i
-
+    filename = "../../../../_uchar/vortex/%02d.mhd" % i
     voreen.setPropertyValue("VolumeSave", "outputFilename", filename)
-
     voreen.repaint()
-
     print filename
