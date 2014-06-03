3f6aa50ae601f87debd648cd01903502e63acad4
Joe <Joe@localhost>
2014-06-03 16:31:13 +0100

    remove Slicer's Volumes module

--- VolumeRenderer/Slicer/Modules/Loadable/Volumes/Widgets/qSlicerVolumesModuleWidgetsExport.h
+++
@@ -1,8 +0,0 @@
-/// qSlicerVolumesModuleWidgetsExport.h
-
-#ifndef __qSlicerVolumesModuleWidgetsExport_h
-#define __qSlicerVolumesModuleWidgetsExport_h
-
-#define Q_SLICER_QTMODULES_VOLUMES_WIDGETS_EXPORT
-
-#endif // __qSlicerVolumesModuleWidgetsExport_h
