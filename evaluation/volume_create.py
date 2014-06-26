0a651485a1473529160eb264add87cc8779c7cab
Joe <Joe@localhost>
2014-06-26 17:16:30 +0100

    update voreen workspace

--- evaluation/volume_create.py
+++ evaluation/volume_create.py
@@ -2,14 +2,14 @@
 import voreen
 import voreenqt
 import time
+from time import gmtime, strftime
 
 voreen.info()
 voreenqt.info()
 
-from time import gmtime, strftime
 t = strftime("%Y%m%d_%H%M%S", gmtime())
 voreen.setPropertyValue("VolumeCreate", "regenerate", 0)
-filename = "../../../../random/volume_" + t + ".mhd"
+filename = "../../../../evaluation/volume_" + t + ".mhd"
 print filename
 voreen.setPropertyValue("VolumeSave", "outputFilename", filename)
 voreen.repaint()
